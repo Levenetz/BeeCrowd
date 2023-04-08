f46564e259a164521ef23e3015e8c30d263831aa
Levenetz <victorlevenetz@gmail.com>
2023-04-08 01:44:55 -0300

    Ex1000
    Primeiro Exercicio Basico

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int A = 30;
-        int B = 10;
-        int SOMA = A+B;
-
-        System.out.println("SOMA = " + SOMA);
-    }
-}
