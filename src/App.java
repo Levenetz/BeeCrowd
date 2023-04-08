065debd394aebc3e15328fd94fd29a88fea519d0
Levenetz <victorlevenetz@gmail.com>
2023-04-08 01:39:53 -0300

    Arquivos Criados

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int A = 30;
+        int B = 10;
+        int SOMA = A+B;
+
+        System.out.println("SOMA = " + SOMA);
+    }
+}
